e984cccfefa147acaf5923a359d6629ee10f671d
Alex <makargravanov@gmail.com>
2024-12-06 21:40:27 +0300

    start commit

---
+++ src/main/java/org/example/acquiringmockbackend/AcquiringMockBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.acquiringmockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquiringMockBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcquiringMockBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/acquiringmockbackend/AcquiringMockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.acquiringmockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcquiringMockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
