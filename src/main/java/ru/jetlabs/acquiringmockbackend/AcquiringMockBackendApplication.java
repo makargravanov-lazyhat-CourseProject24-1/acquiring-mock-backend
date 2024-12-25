1e4efcd95b66d4b5dfcc3ee7100b7ba215c7c615
Alex <makargravanov@gmail.com>
2024-12-26 00:42:51 +0300

    fix schedule

--- src/main/java/ru/jetlabs/acquiringmockbackend/AcquiringMockBackendApplication.java
+++ src/main/java/ru/jetlabs/acquiringmockbackend/AcquiringMockBackendApplication.java
@@ -2,8 +2,10 @@ package ru.jetlabs.acquiringmockbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AcquiringMockBackendApplication {
 
     public static void main(String[] args) {
