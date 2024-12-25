fb4d1d0a5e3210300628588186ab77a78d3908fe
Alex <makargravanov@gmail.com>
2024-12-25 10:43:59 +0300

    try to fix cors

--- src/main/java/ru/jetlabs/acquiringmockbackend/config/CorsConfig.java
+++ src/main/java/ru/jetlabs/acquiringmockbackend/config/CorsConfig.java
@@ -10,9 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/", "https://pay.lazyhat.ru/")
-                .allowedOrigins("http://localhost:5173", "https://pay.lazyhat.ru")
+                .allowedOrigins("http://localhost:5173", "https://pay.lazyhat.ru",
+                        "http://localhost:5173/", "https://pay.lazyhat.ru/")
                 .allowedMethods("*")
+                .allowedHeaders("*")  // Добавьте это
                 .allowCredentials(true);
     }
 }
